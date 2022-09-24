0041376ac33f33d070ab5a9a632f97479f24a6cb
Le5ya <liliatolstyh@yandex.ru>
2022-09-24 12:31:13 +0300

    day-2 fetch-response

--- js/bg-modal.js
+++ js/bg-modal.js
@@ -1,10 +1,13 @@
-const modal = document.querySelector('.search-model')
-const modalBtn = document.querySelector('.icon_search')
-const modalClose = modal.querySelector('.search-close-switch');
+const bgModal = () => {
+	const modal = document.querySelector('.search-model')
+	const modalBtn = document.querySelector('.icon_search')
+	const modalClose = modal.querySelector('.search-close-switch');
 
-modalBtn.addEventListener('click', () => {
-	modal.style.display = 'block'
-})
-modalClose.addEventListener('click', () => {
-	modal.style.display = 'none'
-})
+	modalBtn.addEventListener('click', () => {
+		modal.style.display = 'block'
+	})
+	modalClose.addEventListener('click', () => {
+		modal.style.display = 'none'
+	})
+};
+bgModal();
